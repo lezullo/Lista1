279e7fd746d9afec52d8062369bda10f97239223
lezullo <95085100+lezullo@users.noreply.github.com>
2021-11-29 11:45:37 -0300

    Update Ex18Fazer.py

--- Lista1 Python/Ex18Fazer.py
+++ Lista1 Python/Ex18Fazer.py
@@ -4,3 +4,25 @@
 #c. forneça um método andar( ) que simula o ato de dirigir o veículo por uma certa distância, reduzindo nível de combustível no tanque de gasolina;
 #d. forneça um método obterGasolina( ), que retorna o nível atual de combustível;
 #e. forneça um método adicionarGasolina( ), para abastecer o tanque.
+
+class Carro:
+  def __init__(self, consumo):
+    self.consumo = consumo
+    self.nivelCombustivel = 0
+  
+  def andar(self, distancia):
+    temp = distancia/self.consumo
+    self.nivelCombustivel -= temp
+    
+  def obterGasolina(self):
+    return self.nivelCombustivel
+  
+  def adicionarGasolina(self, qtd):
+    self.nivelCombustivel += qtd
+    
+meucarro = Carro(10)
+meucarro.adicionarGasolina(50)
+print("\nNivel atual de combustivel:" ,meucarro.obterGasolina())
+meucarro.andar(300)
+print("Info:",vars(meucarro))
+print("\nNivel atual de combustivel:" ,meucarro.obterGasolina())
